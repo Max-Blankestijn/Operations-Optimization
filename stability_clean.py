6e9b513651d85cec0d9f9909053126f6c6d6e031
Luka Lorenci <32995823+llooo7@users.noreply.github.com>
2026-03-06 15:31:52 +0100

    Un-spaghettified sensitivity analysis code

---
+++ stability_clean.py
@@ -0,0 +1,211 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+from time import time
+
+import csv
+import gurobipy as gp
+from gurobipy import GRB
+from helper import *
+from model import CVRP
+import sys
+
+class StabilityAnalysisOneSyst():
+    def __init__(self,): #initiates the basic case from model.py
+        self.nodes = [1, 2, 3, 4, 5]
+        self.vehicles = [0, 1]
+        self.dimensions = {"length": 12, "width": 8, "height": 8}
+        
+        self.links = create_links_from_coordinates({1:(100,100), 2:(102,160), 3:(20,178), 4:(72,35), 5:(152,28)}) # map coordinates
+        self.boxes = {1: [2,3,4], 2:[4,2,4], 3:[4,3,3], 4:[6,2,3]} #box dimensions, right?
+
+        # Customer Demand
+        # key = box, keys in subdicts are nodes, values are number of boxes at each node
+        self.demand = {1: {2:3, 3:0, 4:3, 5:4},
+                2: {2:1, 3:3, 4:1, 5:3},
+                3: {2:2, 3:4, 4:0, 5:1},
+                4: {2:4, 3:2, 4:0, 5:1}}
+        
+        # Reach for removing boxes
+        self.maximum_reach = [[self.boxes[i][0] for k in self.nodes[1:]] for i in self.boxes.keys()]
+
+        # Fragility, set to 0 for no boxes on top of this box type, higher value means more load bearing capability
+        self.sigma = [self.boxes[i][2] for i in self.boxes.keys()]
+
+        # weight of box of type i
+        density = 1
+        self.p = [density * self.boxes[i][0] * self.boxes[i][1] * self.boxes[i][2] for i in self.boxes.keys()]
+        self.genConstraints()
+
+    def genConstraints(self, Nconstraints=15):
+        # Active Constraints Dictionary from helper.py constraintGenerator function
+        self.constraints = constraintGenerator(range(1, Nconstraints+1))
+        print('constraints', self.constraints)
+
+    def optimize(self, printResults=False):
+        # Problem Creation
+        problem = CVRP("3L_CVRP", self.nodes, self.links, self.vehicles, self.dimensions, self.boxes, self.demand, self.maximum_reach, self.p, self.sigma, constraints=self.constraints)
+        print("Model Created, starting optimization...")
+        
+        # Optimize model
+        problem.model.params.TimeLimit = 120
+        problem.model.optimize()
+        print("Finished optimization")
+
+        used_boxes1 = {1: [],
+                    2: [],
+                    3: [],
+                    4: []}
+
+        used_boxes2 = {1: [],
+                    2: [],
+                    3: [],
+                    4: []}
+        # Print out taken routes by vehicles
+        if (printResults):
+            if problem.model.status == GRB.OPTIMAL:
+                print("\nActive decision variables (d[i,j,v,t] = 1):")
+                for i, j, v, t in problem.d.keys():
+                    if problem.d[i, j, v, t].X > 0.5:  # X gives the value after optimization
+                        print(f"Vehicle {v} travels from node {i} to {j} at stage {t} | {self.links[i, j]}")
+                for x, y, z, i, k, t, v in problem.a.keys():
+                    if problem.a[x, y, z, i, k, t, v].X > 0.5:
+                        if v == 0:
+                            used_boxes1[i].append([x, y, z, k])
+                        if v == 1:
+                            used_boxes2[i].append([x, y, z, k])
+                        print(f"Box of type {i} in vehicle {v} for customer {k} is at xyz: [{x},{y},{z}] at stage {t}")
+            plot_boxes_3d(used_boxes1, self.boxes, self.dimensions)
+            plot_boxes_3d(used_boxes2, self.boxes, self.dimensions)
+        if (problem.model.SolCount > 0):
+            return problem.model.ObjVal
+        return -1
+            
+    def plot_network(self, node_size=300, cmap='tab10', linkDeltas=None, nodeDeltas=None):
+        #plots the network with color coding signifying change
+        fig, ax = plt.subplots(figsize=(6,6))
+        nodes = list(self.nodes)
+        n = len(nodes)
+        if(linkDeltas==None):
+            linkDeltas = np.zeros(len(self.nodes))
+        if (nodeDeltas==None):
+            nodeDeltas = np.zeros(n)
+        angles = np.linspace(0, 2 * np.pi, n, endpoint=False)
+        node_positions = {node: (np.cos(angle), np.sin(angle)) for node, angle in zip(nodes, angles)}
+        
+        norm = colors.Normalize(vmin=min(linkDeltas), vmax=max(linkDeltas))
+        cmap = cm.get_cmap('viridis')
+        linkCols = [cmap(norm(i)) for i in linkDeltas]
+        print(linkCols)
+        
+        norm = colors.Normalize(vmin=min(linkDeltas), vmax=max(linkDeltas))
+        cmap = cm.get_cmap('viridis')
+        linkCols = [cmap(norm(i)) for i in linkDeltas]
+        
+        seen = set()
+        val = 0
+        for (i,j), info in self.links.items():
+            if i!=j:
+                key = tuple(sorted((i,j)))
+                if key in seen: continue
+                else: seen.add(key)
+                xi, yi = node_positions[i]
+                xj, yj = node_positions[j]
+                ax.plot([xi, xj], [yi, yj], color=linkCols[val], zorder=1)
+                mx, my = (xi + xj) / 2, (yi + yj) / 2
+                ax.text(mx, my, f'{linkDeltas[val]:.5f}', color='black', fontsize=8, ha='center', va='center', zorder=3)
+                val += 1
+        
+        xs = [node_positions[n][0] for n in nodes]
+        ys = [node_positions[n][1] for n in nodes]
+        for idx, node in enumerate(nodes):
+            ax.scatter(node_positions[node][0], node_positions[node][1], s=node_size, color='red', zorder=2) 
+            ax.text(node_positions[node][0],node_positions[node][1], str(node), color='black', ha='center', zorder=4)
+        ax.set_aspect('equal')
+        ax.axis('off')
+        return ax
+    
+    def link_v_distance(self, step=0.2, noSteps=1):
+        #steps = np.linspace(1, 1+noSteps*step, num=noSteps)
+        steps = [ step+1 ]
+        stabilities = []
+        for step in steps:
+            for (i,j), info in self.links.items():
+                if (i != j):
+                    info['distance'] *= step
+                    self.links[(j,i)]['distance'] *= step
+                    stabilities.append(self.optimize()) #currently this only works with with a single variation
+                    info['distance'] /= step
+                    self.links[(j,i)]['distance'] /= step                    
+        return stabilities/np.mean(stabilities)
+    
+    def vary_demands(self, step=-0.1, noSteps = 5):
+        steps = np.linspace(1-step*noSteps/2, 1-step*noSteps/2, noSteps)
+        stabilities = [ ]
+        ogDemand = self.demand
+        i = 0
+        for step in steps:
+            i += 1
+            stabLine = [ 0 ]
+            for node in self.demand:
+                for item in self.demand[node]:
+                    self.demand[node][item] *= step
+                    self.demand[node][item] = int(self.demand[node][item])
+                stabLine.append(self.optimize())
+                self.demand = ogDemand
+            stabilities.append([stabLine])
+            self.status(i, noSteps)
+        return stabilities
+
+    def constraintCost(self, totalN = 19, plot=False):
+        costs = []
+        for i in range(totalN):
+            self.genConstraints(i)
+            print(i)
+            costs.append(self.optimize())
+        if (plot):
+            self.plotGraph(costs)
+        return costs
+        
+    def plotGraph(x, xlabel="x", ylabel="y"): #custom graph plot funct for plot setting consistency 
+        plt.plot(x)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+    
+    def status(done, total):
+        print(str(done) + " out of " + str(total) + " done, " + str(done/total * 100) + "%")
+
+    def vary_reach(self, step = 2, noSteps = 1):
+        #steps = np.linspace(1-step*noSteps/2, 1-step*noSteps/2, noSteps)
+        steps = [ 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4, 0.45, 0.5] #the way to actually do this is go from 1 to such that maximum would reach across the full vehicle, right?
+        costs = []
+        for step in steps:
+            ogReach = self.maximum_reach
+            self.maximum_reach = [[self.boxes[i][0]*step for k in self.nodes[1:]] for i in self.boxes.keys()]
+            costs.append(self.optimize())
+            self.maximum_reach = ogReach
+        return steps, costs
+    
+    def vary_sigma(self, step = 2, noSteps = 1):
+        #steps = np.linspace(1-step*noSteps/2, 1-step*noSteps/2, noSteps)
+        steps = [ 1, 2, 3 ] #the way to actually do this is go from 1 to such that maximum would reach across the full vehicle, right?
+        costs = []
+        for step in steps:
+            ogSigma = self.sigma
+            self.sigma = [self.boxes[i][2]*step for i in self.boxes.keys()]
+            costs.append(self.optimize())
+            self.sigma = ogSigma
+        return costs
+
+
+if __name__ == "__main__":
+    start_time = time()
+    sys = StabilityAnalysisOneSyst()
+    #costs = sys.vary_demands()
+    steps, costs = sys.vary_reach()
+    #sys.plot_network(nodeDeltas=costs)
+    print(costs)
+    print("Run time: " + str(time() - start_time))
+    plt.plot(steps, costs)
+    plt.show()
